f94b43d9462524277698c56ead9c44469bcee80f
Daniel Luna <daniellunasc@icloud.com>
2018-11-10 13:41:37 -0200

    Added: Basic music controls

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, url_for, render_template
+from flask import request, send_file
+import json
+import glob
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+   musicList = glob.glob("static/musics/*.mp3")
+
+   musicJ = [{'filename': mi.split("/")[-1],
+              'fileURL': url_for('sounds', music=mi)} for mi in musicList]
+   return render_template("home.html", musicJ=musicJ)
+
+
+@app.route("/sounds")
+def sounds():
+  music = request.args["music"]
+  return send_file(music, mimetype="audio/mp3")
 
 if(__name__ == "__main__"):
     app.run()
